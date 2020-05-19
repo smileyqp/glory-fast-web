df4d207ded1c731c7d2143e895620dce9a691674
yqp <468417415@qq.com>
2020-05-19 15:42:44 +0800

    glory fast web

---
+++ src/models/login.js
@@ -0,0 +1,88 @@
+import { routerRedux } from 'dva/router';
+import { stringify } from 'qs';
+import { fakeAccountLogin, getFakeCaptcha } from '@/services/api';
+import { setAuthority,setUserInfo,setToken } from '@/utils/authority';
+import { getPageQuery } from '@/utils/utils';
+import { reloadAuthorized } from '@/utils/Authorized';
+
+export default {
+  namespace: 'login',
+
+  state: {
+    status: undefined,
+  },
+
+  effects: {
+    *login({ payload }, { call, put }) {
+      const response = yield call(fakeAccountLogin, payload);
+      yield put({
+        type: 'changeLoginStatus',
+        payload: response,
+      });
+      console.log(response)
+      // Login successfully
+      if (response.ok === true) {
+        setAuthority(response.result.userInfo.createBy);
+        console.log(response)
+        setUserInfo(response.result.userInfo)
+        setToken(response.result.token)
+        reloadAuthorized();
+        const urlParams = new URL(window.location.href);
+        const params = getPageQuery();
+        let { redirect } = params;
+        if (redirect) {
+          const redirectUrlParams = new URL(redirect);
+          if (redirectUrlParams.origin === urlParams.origin) {
+            redirect = redirect.substr(urlParams.origin.length);
+            if (window.routerBase !== '/') {
+              redirect = redirect.replace(window.routerBase, '/');
+            }
+            if (redirect.match(/^\/.*#/)) {
+              redirect = redirect.substr(redirect.indexOf('#') + 1);
+            }
+          } else {
+            redirect = null;
+          }
+        }
+        yield put(routerRedux.replace(redirect || '/'));
+      }
+    },
+
+    *getCaptcha({ payload }, { call }) {
+      yield call(getFakeCaptcha, payload);
+    },
+
+    *logout(_, { put }) {
+      yield put({
+        type: 'changeLoginStatus',
+        payload: {
+          status: false,
+          currentAuthority: 'guest',
+        },
+      });
+      reloadAuthorized();
+      const { redirect } = getPageQuery();
+      // redirect
+      if (window.location.pathname !== '/user/login' && !redirect) {
+        yield put(
+          routerRedux.replace({
+            pathname: '/user/login',
+            search: stringify({
+              redirect: window.location.href,
+            }),
+          })
+        );
+      }
+    },
+  },
+
+  reducers: {
+    changeLoginStatus(state, { payload }) {
+      return {
+        ...state,
+        status: payload.status,
+        type: payload.type,
+      };
+    },
+  },
+};
---
+++ src/utils/request.js
@@ -0,0 +1,182 @@
+/**
+ * request 网络请求工具
+ * 更详细的api文档: https://bigfish.alipay.com/doc/api#request
+ */
+import {getToken} from './authority'
+import { extend } from 'umi-request';
+import { notification } from 'antd';
+import axios from 'axios';
+import router from 'umi/router';
+
+const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+};
+
+function checkcode(status){
+  if (status === 401) {
+    // @HACK
+    /* eslint-disable no-underscore-dangle */
+    window.g_app._store.dispatch({
+      type: 'login/logout',
+    });
+    return;
+  }
+  // environment should not be used
+  if (status === 403) {
+    router.push('/exception/403');
+    return;
+  }
+  if (status <= 504 && status >= 500) {
+    router.push('/exception/500');
+    return;
+  }
+  if (status >= 404 && status < 422) {
+    router.push('/exception/404');
+  }
+}
+
+const checkStatus = response => {
+  console.log('0000000000000')
+  console.log(response)
+  if (response.status === 299) {
+    message.destroy();
+    message.error('请先登录');
+    // 未登录
+    router.push('/user/login');
+  }
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const errortext = codeMessage[response.status] || response.statusText;
+  notification.error({
+    message: `请求错误 ${response.status}: ${response.url}`,
+    description: errortext,
+  });
+  const error = new Error(errortext);
+  error.name = response.status;
+  error.response = response;
+  throw error;
+};
+
+/**
+ * 异常处理程序
+ */
+// const errorHandler = error => {
+//   const { response = {} } = error;
+//   const errortext = codeMessage[response.status] || response.statusText;
+//   const { status, url } = response;
+
+//   if (status === 401) {
+//     notification.error({
+//       message: '未登录或登录已过期，请重新登录。',
+//     });
+//     // @HACK
+//     /* eslint-disable no-underscore-dangle */
+//     window.g_app._store.dispatch({
+//       type: 'login/logout',
+//     });
+//     return;
+//   }
+//   notification.error({
+//     message: `请求错误 ${status}: ${url}`,
+//     description: errortext,
+//   });
+//   // environment should not be used
+//   if (status === 403) {
+//     router.push('/exception/403');
+//     return;
+//   }
+//   if (status <= 504 && status >= 500) {
+//     router.push('/exception/500');
+//     return;
+//   }
+//   if (status >= 404 && status < 422) {
+//     router.push('/exception/404');
+//   }
+// };
+
+// /**
+//  * 配置request请求时的默认参数
+//  */
+// const request = extend({
+//   errorHandler, // 默认错误处理
+//   credentials: 'include', // 默认请求是否带上cookie
+// });
+
+
+
+// export default request;
+export default function request(url, option) {
+  console.log(url)
+  console.log(option)
+  const options = {
+    ...option,
+  };
+  const authorized = getToken()||'';
+
+  const defaultOptions = {
+    credentials: 'include',
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Authorization':authorized
+    },
+  };
+  const newOptions = { ...defaultOptions, ...options };
+  console.log(newOptions)
+  if (
+    newOptions.method === 'POST' ||
+    newOptions.method === 'PUT' ||
+    newOptions.method === 'DELETE'||
+    newOptions.method === 'PATCH'||
+    newOptions.method === 'GET'
+  ) {
+    newOptions.headers = {
+      ...newOptions.headers,
+    };
+    console.log(newOptions)
+    // newOptions.data = qs.stringify(newOptions.body || newOptions.data, {
+    //   allowDots: true,
+    // });
+    newOptions.data = {
+      ...newOptions.data,
+      // timestamp: Date.parse(new Date()) / 1000,
+    };
+  }
+
+  return (
+    axios('http://47.104.190.138:9898/glory-fast'+url, newOptions)
+      .then(checkStatus)
+      // .then(response => cachedSave(response, hashcode))
+      .then(response => {
+        // DELETE and 204 do not return data by default
+        // using .json will report an error.
+        if (newOptions.method === 'DELETE' || response.status === 204) {
+          return response.data;
+        }
+        
+        return response.data;
+      })
+      .catch(e => {
+        console.log(e)
+        console.log(e.name)
+        const status = e.name;
+        
+      })
+  );
+}
